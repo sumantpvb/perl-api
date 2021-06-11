b78104d92f99a8da556cd359bfebc60525f3a0e6
sumantpvb <85758107+sumantpvb@users.noreply.github.com>
2021-06-12 00:57:11 +0530

    added get method

---
+++ Controller.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl 
+
+use Customers;
+use JSON::Fast;
+
+my $file = 'input.csv';
+
+app.config.default-content-type = 'application/json';
+
+get '/customers/schedule' => sub () {
+	open INFILE, "$file" ; 
+	@data = <INFILE> ; 
+	close INFILE ;
+
+    return to-json @data;
+}
